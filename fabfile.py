74a9e7b469decab28be850aabef38f61e440a3f5
Marios Isaakidis <misaakidis@yahoo.gr>
2014-06-28 17:53:19 +0300

    before merging

--- fabfile.py
+++ fabfile.py
@@ -1,19 +1,25 @@
-from fabric.api import run
+from fabric.api import run, hide, settings
 
-def host_type():
-	run('uname -a')
+#env.user = 'root'
+#env.hosts = 'root@192.168.88.251'
 
-def host_mem():
-    run("free -t -m")
-    
-def host_arp():
-    run("cat /proc/net/arp")
+with hide('output'):
+	with settings(host_string='192.168.88.251'):
 
-def ovs_show_cfg():
-	run('ovs-vsctl show')	
+		def host_type():
+			return run('uname -a')
 
-def ovs_add_bridge(name):
-	run("ovs-vsctl add-br %s" % name)
+		def host_mem():
+		    return run("free -t -m")
+		    
+		def host_arp():
+		    return run("cat /proc/net/arp")
 
-def ovs_del_bridge(name):
-    run("ovs-vsctl del-br %s" % name) 
+		def ovs_show_cfg():
+			return run('ovs-vsctl show')	
+
+		def ovs_add_bridge(name):
+			return run("ovs-vsctl add-br %s" % name)
+
+		def ovs_del_bridge(name):
+		    return run("ovs-vsctl del-br %s" % name) 
