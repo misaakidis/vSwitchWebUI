6438b697ef8580cca7328bbac6f6dc1cf83b963a
Marios Isaakidis <misaakidis@yahoo.gr>
2014-06-28 15:36:16 +0300

    Add logging, print uname() from remote machine with test.py

--- test.py
+++ test.py
@@ -2,16 +2,18 @@ import subprocess
 import sys
 
 HOST="root@192.168.88.251"
-# Ports are handled in ~/.ssh/config since we use OpenSSH
-COMMAND="uname -a"
 
-ssh = subprocess.Popen(["ssh", "%s" % HOST, COMMAND],
-                       shell=False,
-                       stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE)
-result = ssh.stdout.readlines()
-if result == []:
-    error = ssh.stderr.readlines()
-    print >>sys.stderr, "ERROR: %s" % error
-else:
-    print result
+def uname():
+	# Ports are handled in ~/.ssh/config since we use OpenSSH
+	COMMAND="uname -a"
+
+	ssh = subprocess.Popen(["ssh", "%s" % HOST, COMMAND],
+	                       shell=False,
+	                       stdout=subprocess.PIPE,
+	                       stderr=subprocess.PIPE)
+	result = ssh.stdout.readlines()
+	if result == []:
+	    error = ssh.stderr.readlines()
+	    return "ERROR: %s" % error
+	else:
+	    return result
--- vSwitchWebUI.py
+++ vSwitchWebUI.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask.ext.httpauth import HTTPDigestAuth
+from test import uname
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bad key'
 auth = HTTPDigestAuth()
+
+if not app.debug:
+    import logging
+    file_handler = logging.FileHandler('./vSwitch.log')
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.WARNING)
+    app.logger.addHandler(file_handler)
 
 users = {
     "admin": "admin",
@@ -11,7 +20,11 @@ users = {
 
 @auth.error_handler
 def auth_error():
-    return "Access Denied"
+    return "Authentication Required"
+
+#@app.errorhandler(404)
+#def page_not_found(e):
+#    return render_template('404.html'), 404
 
 @auth.get_password
 def get_pw(username):
@@ -23,6 +36,22 @@ def get_pw(username):
 @auth.login_required
 def index():
     return "Hello, %s!" % auth.username()
+
+@app.route('/logout/')
+@auth.login_required
+def logout():
+    return "Hello, %s!" % auth.username()
+
+@app.route('/api/v1.0/')
+@auth.login_required
+def getApiRoot():
+	return "API root, access granted to %s" % auth.username()
+
+@app.route('/api/v1.0/uname/', methods=['GET'])
+@auth.login_required
+def getUname():
+	return jsonify(uname=uname())
+
 
 if __name__ == '__main__':
     app.run()
