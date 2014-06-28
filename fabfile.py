2891c8ba06d2df96cca4295b774966a8da6c3c8a
Pavlos Kleanthous <pkleanthous@pkleanthous-MBP.local>
2014-06-28 17:51:12 +0300

    interfaces

--- fabfile.py
+++ fabfile.py
@@ -12,8 +12,19 @@ def host_arp():
 def ovs_show_cfg():
 	run('ovs-vsctl show')	
 
-def ovs_add_bridge(name):
-	run("ovs-vsctl add-br %s" % name)
+def ovs_add_bridge(br_name):
+	run("ovs-vsctl add-br %s" % br_name)
 
-def ovs_del_bridge(name):
-    run("ovs-vsctl del-br %s" % name) 
+def ovs_add_port(br_name, p_name):
+    run("ovs-vsctl add-port %s %s" % (br_name, p_name)) 
+
+def ovs_add_port_vlan(br_name, p_name, vlan):
+    run("ovs-vsctl add-port %s %s tag= %s " % (br_name, p_name, vlan)) 
+
+def ovs_del_bridge(br_name):
+    run("ovs-vsctl add-port %s" % br_name) 
+
+def ovs_list_ports(br_name):
+    run("ovs-vsctl list-ports %s" % br_name)
+    
+    
