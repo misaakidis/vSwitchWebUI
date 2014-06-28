5ce46edf22207a891c442d26190e8335f371b0fe
Pavlos Kleanthous <pkleanthous@pkleanthous-MBP.local>
2014-06-28 16:53:10 +0300

    17:00

--- fabfile.py
+++ fabfile.py
@@ -3,5 +3,18 @@ from fabric.api import run
 def host_type():
 	run('uname -a')
 
+def host_mem():
+    run("free -t -m")
+    
+def host_arp():
+    run("cat /proc/net/arp")
+
 def ovs_show_cfg():
 	run('ovs-vsctl show')	
+
+def ovs_add_bridge():
+	run("ovs-vsctl add-br {name}!").format(name=name)
+
+def ovs_del_bridge():
+    name('myusername')
+    run('ovs-vsctl del-controller' % name)    
